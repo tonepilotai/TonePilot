e919af1f0e0e2242588d8abb0cecad3d04c7f9c7
TonePilot <tonepilot@outlook.com>
2025-05-22 22:40:40 +0100

    Delete Components directory

--- Components/Editor.tsx
+++
@@ -1,4 +0,0 @@
-
-export default function Editor({ value, onChange }: { value: string; onChange: (val: string) => void }) {
-  return <textarea value={value} onChange={e => onChange(e.target.value)} />;
-}
--- Components/TokenGate.tsx
+++
@@ -1,5 +0,0 @@
-
-export default function TokenGate({ children }: { children: React.ReactNode }) {
-  const hasToken = true;
-  return <>{hasToken ? children : <p>Access requires $TONE token</p>}</>;
-}
