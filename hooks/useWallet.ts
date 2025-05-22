c3c32b055f28f1fc3d81f3dbc5792cb446c4d47b
TonePilot <tonepilot@outlook.com>
2025-05-22 22:38:07 +0100

    Add files via upload

--- hooks/useWallet.ts
+++ hooks/useWallet.ts
@@ -1,1 +1,13 @@
 
+import { useEffect, useState } from 'react';
+import { connectWallet } from '../lib/wallet';
+
+export default function useWallet() {
+  const [account, setAccount] = useState<string | null>(null);
+
+  useEffect(() => {
+    connectWallet().then(setAccount);
+  }, []);
+
+  return account;
+}
